34507ceb48dddbcc024ba24bb3c289f5cd02fb8a
Sei Kataoka <sei@uniba.jp>
2017-03-22 16:09:16 +0900

    add database credentials for rds

--- deploy/migrate.sh
+++ deploy/migrate.sh
@@ -2,9 +2,6 @@
 set -x
 
 LC=$(git rev-parse --short HEAD)
-
 kubectl delete jobs/setup || true
-
 sed "s/webapp:LAST_COMMIT/webapp:$LC/g" kube/jobs/setup-job.yaml > setup-job.yaml.tmp
-
 kubectl create -f setup-job.yaml.tmp && rm setup-job.yaml.tmp
